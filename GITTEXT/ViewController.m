3e1bfabcb3295cb669564b073b7718a1a04469be
mac <mac@laoshidekejian2.local>
2016-04-25 23:12:53 +0800

    asdfadsf

--- GITTEXT/ViewController.m
+++ GITTEXT/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"回家了了额额 额");
 }
 
 - (void)didReceiveMemoryWarning {
